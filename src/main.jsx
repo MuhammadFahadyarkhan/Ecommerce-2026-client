5f02855868a3e530236ca29470c56fee98c218aa
fahad yar khan <muhammadfahadyarkhan@gmail.com>
2026-09-21 02:09:05 +0500

    Configure live server backend URL

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import { UserProvider } from './context/UserContext.jsx'
 import { ProductProvider } from './context/ProductContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 
-export const server = "http://localhost:5000";
+export const server = "https://ecommerce-2026-server-production.up.railway.app";
+// export const server = "http://localhost:5000";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
